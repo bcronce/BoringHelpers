0448c51d91b92ab0d0526fa0ca904e4bbfdb51c2
Benjamin Cronce <bcronce@gmail.com>
2019-06-22 14:58:33 -0500

    Start of unit tests

---
+++ BoringHelpersTests/Collections/Collection.cs
@@ -0,0 +1,52 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using BoringHelpers.Collections;
+using System;
+
+namespace BoringHelpersTests.Collections
+{
+    public class Collection
+    {
+        [Fact]
+        public void Individual_CannotAdd()
+        {
+            var collection = Individual.Collection<int>(default);
+            Assert.Throws<NotSupportedException>(() => collection.Add(default));
+        }
+
+        [Fact]
+        public void Individual_CannotClear()
+        {
+            var collection = Individual.Collection<int>(default);
+            Assert.Throws<NotSupportedException>(() => collection.Clear());
+        }
+
+        [Fact]
+        public void Individual_CannotRemove()
+        {
+            var collection = Individual.Collection<int>(default);
+            Assert.Throws<NotSupportedException>(() => collection.Remove(default));
+        }
+
+        [Fact]
+        public void Empty_CannotAdd()
+        {
+            var collection = Empty.Collection<int>();
+            Assert.Throws<NotSupportedException>(() => collection.Add(default));
+        }
+
+        [Fact]
+        public void Empty_CannotClear()
+        {
+            var collection = Empty.Collection<int>();
+            Assert.Throws<NotSupportedException>(() => collection.Clear());
+        }
+
+        [Fact]
+        public void Empty_CannotRemove()
+        {
+            var collection = Empty.Collection<int>();
+            Assert.Throws<NotSupportedException>(() => collection.Remove(default));
+        }
+    }
+}
---
+++ BoringHelpersTests/Collections/Enumerable.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using BoringHelpers.Collections;
+
+namespace BoringHelpersTests.Collections
+{
+    public class Enumerable
+    {
+        [Fact]
+        public void Empty_HasNone() => Assert.Empty(Empty.Enumerable<string>());
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(42)]
+        public void Single_HasOne(int input) => Assert.Single(Individual.Enumerable(input), input);
+    }
+}

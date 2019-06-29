6f4926b650b6f8d73e1416d1befda8afab122325
Benjamin Cronce <bcronce@gmail.com>
2019-06-29 14:25:07 -0500

    List interface unit tests

---
+++ BoringHelpersTests/Collections/List.cs
@@ -0,0 +1,60 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using BoringHelpers.Collections;
+using System;
+
+namespace BoringHelpersTests.Collections
+{
+    public class List
+    {
+        [Fact]
+        public void Individual_CannotInsert()
+        {
+            var list = Individual.List<int>(default);
+            Assert.Throws<NotSupportedException>(() => list.Insert(0, default));
+        }
+
+        [Fact]
+        public void Individual_CannotRemove()
+        {
+            var list = Individual.List<int>(default);
+            Assert.Throws<NotSupportedException>(() => list.RemoveAt(0));
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(42)]
+        public void Individual_IndexOf(int input)
+        {
+            var list = Individual.List(input);
+            const int expectedIndex = 0;
+            Assert.Equal(expectedIndex, list.IndexOf(input));
+        }
+
+        [Fact]
+        public void Empty_CannotInsert()
+        {
+            var list = Empty.List<int>();
+            Assert.Throws<NotSupportedException>(() => list.Insert(0, default));
+        }
+
+        [Fact]
+        public void Empty_CannotRemove()
+        {
+            var list = Empty.List<int>();
+            Assert.Throws<NotSupportedException>(() => list.RemoveAt(0));
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(42)]
+        public void Empty_IndexOf(int input)
+        {
+            var list = Empty.List<int>();
+            const int notFound = -1;
+            Assert.Equal(notFound, list.IndexOf(input));
+        }
+    }
+}
